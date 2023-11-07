9b5900877fdba47ed9cd12d05184e7d45005b383
Pauline Cenizal <paulinestyles1@gmail.com>
2023-11-06 17:19:11 -0800

    swap the caption

--- lab7-template/image_swap.js
+++ lab7-template/image_swap.js
@@ -1,15 +1,29 @@
 "use strict";
 
-const fadeOutImageCallback = () => {
+const fadeOutImageCallback = (link) => {
 	// TODO
+	// change text caption
+	// change attribute for the src
+	// jquery fadeOut and jquery fadein
+
+	console.log('link in fadeOutImageCallback', link);
+	console.log('link.title in fadeOutImageCallback', link.title);
+	console.log('link.src in fadeOutImageCallback', link.children[0].src);
+	const caption = $("#caption");
+	caption.text(link.title);
+	caption.fadeIn(1000);
+	
+	const image = $("#image");
+	image.src = link.children[0].src
+	image.fadeIn(1000);
 };
 
 const handleImageSwap = evt => {
 	const link = evt.currentTarget;
-	
+	console.log('link in handleImageSwap', link);
 	// TODO: handle image swap
 	$("#caption").fadeOut(1000);
-    $("#image").fadeOut(1000, fadeOutImageCallback //callback function to swap the image);
+	$("#image").fadeOut(1000, () => fadeOutImageCallback(link)) //callback function to swap the image
 };
 
 $(document).ready( () => {
@@ -25,6 +39,7 @@ $(document).ready( () => {
 
 	// TODO: use jQuery click event like document.addEventListener("DOMContentLoaded", () => {}
 	$("#image_list a").click(handleImageSwap);
+
 	
 	// move focus to first thumbnail
 	$("li:first-child a").focus();
